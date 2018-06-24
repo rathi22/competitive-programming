cbcf0777b32900e1b645d1ef7b0c7ccbc0081c71
rathi22 <rajatrathidgr81@gmail.com>
2018-06-24 21:25:36 +0530

    standard template

---
+++ template.cpp
@@ -0,0 +1,19 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+typedef long double ld;
+#define sc(x) scanf("%lld",&x)
+#define sc2(x,y) scanf("%lld %lld",&x,&y)
+#define sc3(x,y,z) scanf("%lld %lld %lld",&x,&y,&z)
+#define pb push_back
+#define mp make_pair
+#define ip pair<ll,ll>
+#define ff first
+#define ss second
+#define MAXN 200005
+#define MOD 1000000007
+#define INF 1e18
+
+int main(){
+	
+}
